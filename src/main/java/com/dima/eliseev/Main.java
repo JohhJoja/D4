1b73ddb94eb7be416da3aee81a9a031aef13b1f8
JohhJoja <08022002dimaeliseew@gmail.com>
2025-03-24 12:30:38 +0300

    Connect to DB and create query

--- src/main/java/com/dima/eliseev/Main.java
+++ src/main/java/com/dima/eliseev/Main.java
@@ -1,11 +1,27 @@
 package com.dima.eliseev;
 
+import java.sql.*;
+
 public class Main {
 
     public static void main(String[] args) {
+        final String URL = "jdbc:postgresql://localhost:5433/JDBC";
+        final String USER = "postgres";
+        final String PASSWORD = "1";
 
-       // DB_data_generator.GENERATE();
+        try(Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            Statement statement = connection.createStatement()){
+            statement.executeUpdate("drop table Dimasik");
+            statement.executeUpdate("create table Dimasik (id bigserial primary key, name varchar(50), age int)");
+            statement.executeUpdate("insert into Dimasik (name, age) values ('Dimasik', 21)");
+            ResultSet resultSet = statement.executeQuery("select * from Dimasik");
+            while (resultSet.next()){
+                System.out.println(resultSet.getInt(1));
+                System.out.println(resultSet.getString(2));
+            }
 
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
-
 }
