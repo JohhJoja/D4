5c88426b3fe5e0d24791be8226dd93619b5e60db
JohhJoja <08022002dimaeliseew@gmail.com>
2025-03-24 13:03:07 +0300

    repeating

--- src/main/java/com/dima/eliseev/Main.java
+++ src/main/java/com/dima/eliseev/Main.java
@@ -4,24 +4,20 @@ import java.sql.*;
 
 public class Main {
 
-    public static void main(String[] args) {
-        final String URL = "jdbc:postgresql://localhost:5433/JDBC";
-        final String USER = "postgres";
-        final String PASSWORD = "1";
+    public static void main(String[] args) throws SQLException {
 
-        try(Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        String URL = "jdbc:postgresql://localhost:5433/JDBC";
+        String NAME = "postgres";
+        String PASSWORD = "1";
+
+        try (Connection connection = DriverManager.getConnection(URL, NAME, PASSWORD);
             Statement statement = connection.createStatement()){
-            statement.executeUpdate("drop table Dimasik");
-            statement.executeUpdate("create table Dimasik (id bigserial primary key, name varchar(50), age int)");
-            statement.executeUpdate("insert into Dimasik (name, age) values ('Dimasik', 21)");
             ResultSet resultSet = statement.executeQuery("select * from Dimasik");
             while (resultSet.next()){
-                System.out.println(resultSet.getInt(1));
+                System.out.println(resultSet.getString(1));
                 System.out.println(resultSet.getString(2));
             }
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
         }
+
     }
 }
