73defde585484af99ed46a7946d47034efb34ecf
JohhJoja <08022002dimaeliseew@gmail.com>
2025-03-24 16:34:46 +0300

    Writing and reading image

--- src/main/java/com/dima/eliseev/Main.java
+++ src/main/java/com/dima/eliseev/Main.java
@@ -2,10 +2,7 @@ package com.dima.eliseev;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.sql.*;
 
 public class Main {
@@ -19,17 +16,24 @@ public class Main {
         try (Connection connection = DriverManager.getConnection(URL, NAME, PASSWORD);
             Statement statement = connection.createStatement()){
            // statement.executeUpdate("create table blobi (id bigserial primary key, name varchar(50), img bytea)");
-
             BufferedImage img = ImageIO.read(new File("E:\\Java\\deeplomka\\JDBC\\src\\main\\resources\\img.jpg"));
+            ByteArrayOutputStream BOAS = new ByteArrayOutputStream();
+            ImageIO.write(img, "jpg", BOAS);
+            byte[] bytedImg = BOAS.toByteArray();
 
-            ByteArrayOutputStream babun = new ByteArrayOutputStream();
-            ImageIO.write(img, "jpg", babun);
-            byte[] imageBytes = babun.toByteArray();
+            PreparedStatement preparedStatement1 = connection.prepareStatement("insert into blobi (name, img) values (?,?)");
+            preparedStatement1.setString(1,"Uranus");
+            preparedStatement1.setBytes(2, bytedImg);
+           // preparedStatement.execute();
+            PreparedStatement preparedStatement2 = connection.prepareStatement("select img from blobi where id = 2");
+            ResultSet resultSet = preparedStatement2.executeQuery();
+            resultSet.next();
+            byte[] r_byteImg = resultSet.getBytes("img");
+            File OF = new File("E:\\Java\\deeplomka\\JDBC\\src\\main\\resources\\saved.jpg");
+            ByteArrayInputStream bais = new ByteArrayInputStream(r_byteImg);
+            BufferedImage retrievedImage = ImageIO.read(bais);
+            ImageIO.write(retrievedImage, "jpg", OF);
 
-            PreparedStatement preparedStatement = connection.prepareStatement("insert into blobi (name, img) values (?,?)");
-            preparedStatement.setString(1,"Dimasik");
-            preparedStatement.setBytes(2,imageBytes);
-            preparedStatement.execute();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
