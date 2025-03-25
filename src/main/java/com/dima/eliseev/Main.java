fe8de20cf1e902dba8127019426246e521ea4799
JohhJoja <08022002dimaeliseew@gmail.com>
2025-03-25 15:36:11 +0300

    Can scroll down and top

--- src/main/java/com/dima/eliseev/Main.java
+++ src/main/java/com/dima/eliseev/Main.java
@@ -13,8 +13,35 @@ public class Main {
         String NAME = "postgres";
         String PASSWORD = "1";
 
-        try (Connection connection = DriverManager.getConnection(URL, NAME, PASSWORD); Statement statement = connection.createStatement()){
-            
+        try (Connection connection = DriverManager.getConnection(URL, NAME, PASSWORD);
+             Statement statement = connection.createStatement()){
+
+            Statement stat = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+
+            ResultSet resultSet = stat.executeQuery("Select * from blobi");
+            if (resultSet.next()){
+                System.out.println(resultSet.getString(1) + " " + resultSet.getString(2));
+            }
+            if (resultSet.previous()){
+                System.out.println(resultSet.getString(1) + " " + resultSet.getString(2));
+            }
         }
+//             PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM test1()")) {
+//
+//            boolean hasNext = preparedStatement.execute();
+//            ResultSet resultSet = preparedStatement.getResultSet();
+//            while (hasNext){
+//                while (resultSet.next()) {
+//                    System.out.println(resultSet.getString(1) + " " + resultSet.getString(2));
+//                }
+//                hasNext = preparedStatement.getMoreResults();
+//            }
+
+
+
+    catch (SQLException e) {
+            e.printStackTrace();
+        }
+
     }
 }
