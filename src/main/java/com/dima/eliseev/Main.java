7fabbf813533e27d7b5c213648c33a801f07e94f
JohhJoja <08022002dimaeliseew@gmail.com>
2025-03-25 10:19:49 +0300

    delete useless futches

--- src/main/java/com/dima/eliseev/Main.java
+++ src/main/java/com/dima/eliseev/Main.java
@@ -13,31 +13,6 @@ public class Main {
         String NAME = "postgres";
         String PASSWORD = "1";
 
-//        try (Connection connection = DriverManager.getConnection(URL, NAME, PASSWORD);
-//            Statement statement = connection.createStatement()){
-//           // statement.executeUpdate("create table blobi (id bigserial primary key, name varchar(50), img bytea)");
-//            BufferedImage img = ImageIO.read(new File("E:\\Java\\deeplomka\\JDBC\\src\\main\\resources\\img.jpg"));
-//            ByteArrayOutputStream BOAS = new ByteArrayOutputStream();
-//            ImageIO.write(img, "jpg", BOAS);
-//            byte[] bytedImg = BOAS.toByteArray();
-//
-//            PreparedStatement preparedStatement1 = connection.prepareStatement("insert into blobi (name, img) values (?,?)");
-//            preparedStatement1.setString(1,"Uranus");
-//            preparedStatement1.setBytes(2, bytedImg);
-//           // preparedStatement.execute();
-//            PreparedStatement preparedStatement2 = connection.prepareStatement("select img from blobi where id = 2");
-//            ResultSet resultSet = preparedStatement2.executeQuery();
-//            resultSet.next();
-//            byte[] r_byteImg = resultSet.getBytes("img");
-//            File OF = new File("E:\\Java\\deeplomka\\JDBC\\src\\main\\resources\\saved.jpg");
-//            ByteArrayInputStream bais = new ByteArrayInputStream(r_byteImg);
-//            BufferedImage retrievedImage = ImageIO.read(bais);
-//            ImageIO.write(retrievedImage, "jpg", OF);
-//
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
-
         try (Connection connection = DriverManager.getConnection(URL, NAME, PASSWORD); Statement statement = connection.createStatement()){
             
         }
