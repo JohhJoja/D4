a03e51e90b5f4f815046f1d996fcddd2ad20ee47
JohhJoja <08022002dimaeliseew@gmail.com>
2025-03-26 10:46:52 +0300

    reate my first regex

--- src/main/java/com/dima/eliseev/Main.java
+++ src/main/java/com/dima/eliseev/Main.java
@@ -4,6 +4,8 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.*;
 import java.sql.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
@@ -13,28 +15,37 @@ public class Main {
         String NAME = "postgres";
         String PASSWORD = "1";
 
-        try (Connection connection = DriverManager.getConnection(URL, NAME, PASSWORD);
-             Statement statement = connection.createStatement()){
+//        try (Connection connection = DriverManager.getConnection(URL, NAME, PASSWORD);
+//             Statement statement = connection.createStatement()){
+//
+//            Statement statement1 = connection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+//
+//            connection.setAutoCommit(false);
+//            statement1.executeUpdate("INSERT INTO dimasik (name, age) values ('Dimass', 39)");
+//            Savepoint savepoint = connection.setSavepoint();
+//            statement1.executeUpdate("INSERT INTO dimasik (name, age) values ('Anananananaaaa', 9)");
+//            statement1.executeUpdate("INSERT INTO dimasik (name, age) values ('Chuchpan', 3)");
+//
+//            connection.rollback(savepoint); //откат изменений
+//            connection.commit();
+//
+//        }
+//    catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//
 
-            Statement statement1 = connection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+        REGEX.match_A("My first regular expression aaaaa!");
 
-            connection.setAutoCommit(false);
-            statement1.executeUpdate("INSERT INTO dimasik (name, age) values ('Dimass', 39)");
-            Savepoint savepoint = connection.setSavepoint();
-            statement1.executeUpdate("INSERT INTO dimasik (name, age) values ('Anananananaaaa', 9)");
-            statement1.executeUpdate("INSERT INTO dimasik (name, age) values ('Chuchpan', 3)");
+    }
+}
 
-            connection.rollback(savepoint); //откат изменений
-            connection.commit();
-
+class REGEX{
+    static public void match_A(String ex){
+        Pattern p = Pattern.compile("a");
+        Matcher m = p.matcher(ex);
+        while (m.find()){
+            System.out.println(m.start() + " " + m.group() + " ");
         }
-
-
-
-
-    catch (SQLException e) {
-            e.printStackTrace();
-        }
-
     }
 }
