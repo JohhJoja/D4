adc60f80c43ea15afc6b08f08dbf6cbbfc5259bc
JohhJoja <08022002dimaeliseew@gmail.com>
2025-03-26 13:05:42 +0300

    special characters

--- src/main/java/com/dima/eliseev/Main.java
+++ src/main/java/com/dima/eliseev/Main.java
@@ -35,17 +35,41 @@ public class Main {
 //        }
 //
 
-        REGEX.match_A("My first regular expression aaaaa!");
+        REGEX.match_("a", "aaaaaaa");
+        REGEX.match_("\\Q1+1=2\\E", "1+1=2 экранирует");
+        REGEX.match_("1\\+1=2", "1+1=2 это тоже");
+
+        REGEX.match_("[a-d]", "any letters from a to d");
+        REGEX.match_("\\D", "qwesad1sd5asd4"); ///^d
+
+        REGEX.match_("[0-9]", "F1nd any n4m5r1c");
+        REGEX.match_("\\d", "qwesad1sd5asd4");
+
+        REGEX.match_("[0-3a-b]","Any 75tt5r5 (a-b) and n4mb5rs (0-3)");
+        REGEX.match_("[\\w]","Any1 simbl1");
+
+        REGEX.match_("[\\s]","Find every space");
+        REGEX.match_("[\\S]","Find all except space");
+
+        REGEX.match_("[A-U]", "Find Any Uppercase Letters");
+        REGEX.match_("[a-z}%@=]", "Find lett " +
+                "+ this ch in any position (for example: Alola = @100%");
+        REGEX.match_("gr[ae]y","Can find: gray or grey");
+        REGEX.match_("q[^mmnbvcxzlkjhgfdsapoiuytre]", "qw");
+        /// ^ - нужно указывать в начале, иначе, если будет в другом месте, то будет
+        /// считаться обычным символом
 
     }
 }
 
 class REGEX{
-    static public void match_A(String ex){
-        Pattern p = Pattern.compile("a");
+    static public void match_(String regex, String ex){
+        Pattern p = Pattern.compile(regex);
         Matcher m = p.matcher(ex);
         while (m.find()){
-            System.out.println(m.start() + " " + m.group() + " ");
+            System.out.print(m.start() + " " + m.group() + " ");
         }
+        System.out.println("---------------  "+ex);
     }
 }
+
